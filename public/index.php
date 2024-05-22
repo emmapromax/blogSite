bd12afe103d122298d14e38628ce873f4d362f19
emmapromax <josiahemmanuel687@gmail.com>
2024-05-22 18:30:25 +0100

    my blog

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="fa_icons/css/all.css">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="js/app.js"></script>
+</head>
+<body>
+    <h1 class="bg-red-400">dwdwdw</h1>
+</body>
+</html>
---
+++ src/script.js
@@ -0,0 +1,5 @@
+import axios from "axios";  
+import moment from "moment";
+
+window.axios=axios
+window.moment=moment
